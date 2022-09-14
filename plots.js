03141696ba81b72a9e43b4ac4fc3fc56f797bef8
Ryan Chapman <107438816+WeirdFishez@users.noreply.github.com>
2022-09-14 13:49:07 -0600

    Add files via upload

--- plots.js
+++ plots.js
@@ -1,44 +1,39 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/data.json").then((importedData) => {
-  // console.log(importedData);
-  var data = importedData;
+function init() {
+  var selector = d3.select("#selDataset");
 
-  // Sort the data array using the greekSearchResults value
-  data.sort(function(a, b) {
-    return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+  d3.json("samples.json").then((data) => {
+    console.log(data);
+    var sampleNames = data.names;
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+  })
+}
+
+init();
+
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
+
+
+
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
+
+    Object.entries(result).forEach((key) => {
+      PANEL.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+    });
   });
-
-  // Slice the first 10 objects for plotting
-  data = data.slice(0, 10);
-
-  // Reverse the array due to Plotly's defaults
-  data = data.reverse();
-
-  // Trace1 for the Greek Data
-  var trace1 = {
-    x: data.map(row => row.greekSearchResults),
-    y: data.map(row => row.greekName),
-    text: data.map(row => row.greekName),
-    name: "Greek",
-    type: "bar",
-    orientation: "h"
-  };
-
-  // data
-  var chartData = [trace1];
-
-  // Apply the group bar mode to the layout
-  var layout = {
-    title: "Greek gods search results",
-    margin: {
-      l: 100,
-      r: 100,
-      t: 100,
-      b: 100
-    }
-  };
-
-  // Render the plot to the div tag with id "plot"
-  Plotly.newPlot("plot", chartData, layout);
-});
+} 
+      
+      
+  
---
+++ script.js
@@ -0,0 +1,11 @@
+d3.selectAll("body").on("change", updatePage);
+
+function updatePage() {
+  var dropdownMenu = d3.selectAll("#selectOption").node();
+  var dropdownMenuID = dropdownMenu.id;
+  var selectedOption = dropdownMenu.value;
+
+  console.log(dropdownMenuID);
+  console.log(selectedOption);
+  console.log(d3.selectAll("#menu").node().id);
+};
